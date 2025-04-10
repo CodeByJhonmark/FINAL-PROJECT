b3c7250ce6669f98407e9d1bd1e15b83610772f8
ryu1n <seannorcio4@gmail.com>
2025-04-10 12:26:20 +0800

    updated

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
     "act1" ng act2 ganon tapos yung buttonAct1 palitan din ng buttonAct2 */
     const act1 = document.getElementById('buttonAct1');
     const act2 = document.getElementById('buttonAct2');
+    const act3 = document.getElementById('buttonAct3');
+    const act4 = document.getElementById('buttonAct4');
+    const act5 = document.getElementById('buttonAct5');
+    const act6 = document.getElementById('buttonAct6');
+    const act7 = document.getElementById('buttonAct7');
+    const act8 = document.getElementById('buttonAct8');
+    const act9 = document.getElementById('buttonAct9');
+    const act10 = document.getElementById('buttonAct10');
+    const act11 = document.getElementById('buttonAct11');
+    const act12 = document.getElementById('buttonAct12');
+    const act13 = document.getElementById('buttonAct13');
+    const act14 = document.getElementById('buttonAct14');
+    const act15 = document.getElementById('buttonAct15');
+    const act16 = document.getElementById('buttonAct16');
     const act17 = document.getElementById('buttonAct17');
     const act18 = document.getElementById('buttonAct18');
     const act19 = document.getElementById('buttonAct19');
@@ -62,6 +76,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (act2) {
       act2.addEventListener('click', () => showEditorWithCode('act2'));
+    }
+    if (act3) {
+      act3.addEventListener('click', () => showEditorWithCode('act3'));
+    }
+    if (act4) {
+      act4.addEventListener('click', () => showEditorWithCode('act4'));
+    }
+    if (act5) {
+      act5.addEventListener('click', () => showEditorWithCode('act5'));
+    }
+    if (act6) {
+      act6.addEventListener('click', () => showEditorWithCode('act6'));
+    }
+    if (act8) {
+      act8.addEventListener('click', () => showEditorWithCode('act8'));
+    }
+    if (act7) {
+      act7.addEventListener('click', () => showEditorWithCode('act7'));
+    }
+    if (act9) {
+      act9.addEventListener('click', () => showEditorWithCode('act9'));
+    }
+    if (act10) {
+      act10.addEventListener('click', () => showEditorWithCode('act10'));
+    }
+    if (act11) {
+      act11.addEventListener('click', () => showEditorWithCode('act11'));
+    }
+    if (act12) {
+      act12.addEventListener('click', () => showEditorWithCode('act12'));
+    }
+    if (act13) {
+      act13.addEventListener('click', () => showEditorWithCode('act13'));
+    }
+    if (act14) {
+      act14.addEventListener('click', () => showEditorWithCode('act14'));
+    }
+    if (act15) {
+      act15.addEventListener('click', () => showEditorWithCode('act15'));
+    }
+    if (act16) {
+      act16.addEventListener('click', () => showEditorWithCode('act16'));
     }
     if (act17) {
       act17.addEventListener('click', () => showEditorWithCode('act17'));
